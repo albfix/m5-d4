418ec34903116a9fbf64436b0096ede7233d1963
Alberto Gaias <63840357+albfix@users.noreply.github.com>
2020-06-25 16:48:55 +0200

    get and post now works, next step up/download

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.json()) //SPECIFICARE PER RICEVERE QUANDO SI USA IL METODO POST
+
 const userRouter = require("./src/routes/user")
 
 app.get("/hello", async (req, res) => {
     console.log("funziona")
-    res.status(200).send("funziona res.send")
+    res.status(200).send("funziona res send")
 })
 
 app.use("/users", userRouter)
-app.listen(3000, () => console.log("Server is running"))
+app.listen(4000, () => console.log("Server is running"))
+
+
+/////////////////////////////
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -1,9 +1,37 @@
 const express = require("express")
+const fs = require("fs")
+const path = require("path")
+
+const getUsers = () => {
+    const userJsonPath = path.join(__dirname, "users.json")
+    const userBuffer = fs.readFileSync(userJsonPath)
+    const userString = userBuffer.toString()
+    const users = JSON.parse(userString)
+    return users
+
+}
+
+//AGGIUNGERE AD ARRAY
+const writeUsers = (users) => {
+    const userJsonPath = path.join(__dirname, "users.json")
+    fs.writeFileSync(userJsonPath, JSON.stringify(users))
+}
 
 const userRouter = express.Router()
 
 userRouter.get("/", (req, res) => {
-    res.send("res di userRouter")
+    const users = getUsers()
+    res.send(users)
 })
 
-module.export = userRouter
+userRouter.post("/", (req, res) => {
+    console.log(req.body)
+    const users = getUsers() //PRENDO L'ARRAY
+    users.push(req.body)//AGGIUNGO
+    writeUsers(users)//SALVO NEL DATABASE
+    res.send(req.body)//RESTITUISCO L'ARRAY AGGIORNATO
+})
+
+module.exports = userRouter
+
+//////////////////////
